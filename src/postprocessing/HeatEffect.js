5167aec8f603ffb5337edb11b97d38f065558c9f
Gabriela Seabra <gabriela.lima.se@gmail.com>
2020-12-24 23:16:46 -0300

    blending mode

--- src/postprocessing/HeatEffect.js
+++ src/postprocessing/HeatEffect.js
@@ -3,21 +3,24 @@ import { Effect, BlendFunction } from "postprocessing";
 export class HeatEffect extends Effect {
   constructor() {
     super("HeatEffect", FRAGMENT, {
-      // blendFunction: BlendFunction.SKIP
+      blendFunction: BlendFunction.AVERAGE
     });
   }
 }
 
 const FRAGMENT = `
-#define HEAT_FREQUENCY 50.0
-#define HEAT_SPEED 1.0
+#define HEAT_FREQUENCY 30.0
+#define HEAT_AMPLITUDE 0.08
+#define HEAT_SPEED 5.0
 
 float easeInCubic(float x) {
   return x * x * x;
 }
 
-void mainUv(inout vec2 uv) {
+void mainImage(const in vec4 color, const in vec2 uv, out vec4 outputColor) {
   float y = easeInCubic(uv.y);
-  uv.x += sin(y * HEAT_FREQUENCY + (1.0 - y) * time * HEAT_SPEED) / HEAT_FREQUENCY;
+  vec2 vUv = uv;
+  vUv.x += sin(y * HEAT_FREQUENCY + (time * HEAT_SPEED)) / (HEAT_FREQUENCY * (1.0 / HEAT_AMPLITUDE));
+  outputColor = texture2D(inputBuffer, vUv);
 }
 `;
